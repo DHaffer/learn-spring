f7b60c7dfb955b2075b68b3c2d0c0435bbbb18df
Daryn <dhaffer1991@gmail.com>
2023-08-16 17:31:03 -0500

    Step 06

--- src/main/java/com/springtutorial/learningspringframework/AppGamingBasicJava.java
+++ src/main/java/com/springtutorial/learningspringframework/AppGamingBasicJava.java
@@ -2,15 +2,17 @@ package com.springtutorial.learningspringframework;
 
 import com.springtutorial.learningspringframework.game.GameRunner;
 import com.springtutorial.learningspringframework.game.MarioGame;
+import com.springtutorial.learningspringframework.game.PacManGame;
 import com.springtutorial.learningspringframework.game.SuperContraGame;
 
 public class AppGamingBasicJava {
 
 	public static void main(String[] args) {
 		
-		//var marioGame = new MarioGame();
-		var superContraGame = new SuperContraGame();
-		var gameRunner = new GameRunner(superContraGame);
+		//var game = new MarioGame();
+		//var game = new SuperContraGame();
+		var game = new PacManGame();
+		var gameRunner = new GameRunner(game);
 		gameRunner.run();
 		
 	}
