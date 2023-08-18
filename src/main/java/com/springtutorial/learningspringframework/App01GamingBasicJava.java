13ea2bf2f4978c77f4878c35fabd2faba8a90579
Daryn <dhaffer1991@gmail.com>
2023-08-18 15:18:42 -0500

    Step 08

--- src/main/java/com/springtutorial/learningspringframework/App01GamingBasicJava.java
+++ src/main/java/com/springtutorial/learningspringframework/App01GamingBasicJava.java
@@ -5,7 +5,7 @@ import com.springtutorial.learningspringframework.game.MarioGame;
 import com.springtutorial.learningspringframework.game.PacManGame;
 import com.springtutorial.learningspringframework.game.SuperContraGame;
 
-public class AppGamingBasicJava {
+public class App01GamingBasicJava {
 
 	public static void main(String[] args) {
 		
