6150e53a65924034d04bd3f7516649c4aeb3ce31
Daryn <dhaffer1991@gmail.com>
2023-08-18 16:23:37 -0500

    Step 09

--- src/main/java/com/springtutorial/learningspringframework/App02HelloWorldSpring.java
+++ src/main/java/com/springtutorial/learningspringframework/App02HelloWorldSpring.java
@@ -15,7 +15,7 @@ public class App02HelloWorldSpring {
 		//name - @Bean
 		
 		//3: Retrieving Beans managed by Spring
-		System.out.println(context.getBean("name"));
+		System.out.println(context.getBean("address"));
 		
 	}
 
--- src/main/java/com/springtutorial/learningspringframework/HelloWorldConfiguration.java
+++ src/main/java/com/springtutorial/learningspringframework/HelloWorldConfiguration.java
@@ -3,6 +3,14 @@ package com.springtutorial.learningspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+//Eliminate verbosity with Java Beans
+//Public accessor methods, constructor, equals, hashcode, ToString automatically created
+//Released in JDK 16
+
+
+record Person (String name, int age) { };
+record Address (String firstLine, String city) { };
+
 @Configuration 
 public class HelloWorldConfiguration {
 
@@ -10,5 +18,23 @@ public class HelloWorldConfiguration {
 	public String name() {
 		return "Daryn";
 	}
+	
+	@Bean
+	public int age() {
+		return 30;
+	}
+	
+	@Bean
+	public Person person() {
+		return new Person("John", 25);
+		
+	}
+	
+	@Bean
+	public Address address() {
+		return new Address("Gallatin", "Nashville");
+	}
+	
+	
 	
 }
