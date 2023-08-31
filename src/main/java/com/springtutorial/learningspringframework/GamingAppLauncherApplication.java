e7ce4a32bf1f712cd290e8075c56697764b15adf
Daryn <dhaffer1991@gmail.com>
2023-08-31 14:21:14 -0500

    Sec 3 Step 01

--- src/main/java/com/springtutorial/learningspringframework/GamingAppLauncherApplication.java
+++ src/main/java/com/springtutorial/learningspringframework/GamingAppLauncherApplication.java
@@ -1,6 +1,9 @@
 package com.springtutorial.learningspringframework;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 import com.springtutorial.learningspringframework.game.GameRunner;
 import com.springtutorial.learningspringframework.game.GamingConsole;
@@ -8,19 +11,18 @@ import com.springtutorial.learningspringframework.game.MarioGame;
 import com.springtutorial.learningspringframework.game.PacManGame;
 import com.springtutorial.learningspringframework.game.SuperContraGame;
 
-public class App03GamingSpringBeans {
-
+@Configuration
+@ComponentScan("com.springtutorial.learningspringframework.game")
+public class GamingAppLauncherApplication {
+		
 	public static void main(String[] args) {
 		
 		try (var context = 
-				new AnnotationConfigApplicationContext(GamingConfiguration.class)) {
+				new AnnotationConfigApplicationContext(GamingAppLauncherApplication.class)) {
 			context.getBean(GamingConsole.class).up();
 			
 			context.getBean(GameRunner.class).run();
 		}
-		
-		
-		
 		
 	}
 
